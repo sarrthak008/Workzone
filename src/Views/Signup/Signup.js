1602d9b7d3325007660dff589761ea3dcbe6ea29
sarrthak008 <sarthaknavale08@gmail.com>
2024-09-26 20:49:54 +0530

    add signup form

--- src/Views/Signup/Signup.js
+++ src/Views/Signup/Signup.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import "./Signup.css"
+import SIGNUPIMG from "./Signup.png"
 
 const Signup = () => {
   return (
     <div>
         <Navbar/>
-        signup here
+        <div className='signup-container'>
+              <div className='form-and-img-container'>
+                 <div className='signup-form-container'>
+                   <span className='signup-text'>Signup with workZone.</span>
+                   <span>
+                      <input placeholder='first name'></input>
+                      <input placeholder='last name'></input>
+                   </span>
+                 </div>
+                 <div className='signup-image-container'>
+                    <img src={SIGNUPIMG}></img>
+                 </div>
+              </div>
+        </div>
     </div>
   )
 }
