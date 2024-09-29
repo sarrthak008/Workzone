8e7cf25c149f8bddd36e642967214d614aa69dc0
sarrthak008 <sarthaknavale08@gmail.com>
2024-09-29 11:45:58 +0530

    add jobs card and its image

--- src/Components/FeedCrad/FeedCard.js
+++ src/Components/FeedCrad/FeedCard.js
@@ -1,5 +1,22 @@
 import React from 'react'
 import "./FeedCard.css"
+import { COLORS } from '../../Config/Colors'
+
+import IMG1 from "./assets/pic1.png"
+import IMG2 from "./assets/pic2.png"
+import IMG3 from "./assets/pic3.png"
+import IMG4 from "./assets/pic4.png"
+import IMG5 from "./assets/pic5.png"
+import IMG6 from "./assets/pic6.png"
+import IMG7 from "./assets/pic7.png"
+import IMG8 from "./assets/pic8.png"
+import IMG9 from "./assets/pic9.png"
+import IMG10 from "./assets/pic10.png"
+
+const IMGS = [IMG1,IMG2,IMG3,IMG4,IMG5,IMG6,IMG7,IMG8,IMG9,IMG10]
+
+
+
 
 const FeedCard = ({info}) => {
   return (
@@ -13,6 +30,17 @@ const FeedCard = ({info}) => {
                 <span className='feed-card-email'>{info.email}</span>
              </div>
          </div>
+        <span className='feed-card-email'>{info.title.split("(")[0]}</span>
+        <div className='job-card-image-container'>
+            <div className='image-job-card' style={{
+              backgroundColor:`${COLORS[Math.floor(Math.random()*COLORS.length)]}`
+            }}>
+              <img src={`${IMGS[Math.floor(Math.random()*IMGS.length)]}`}></img>
+            <div className='job-info-overlay'>
+                <span className='company-info'>{info.c_name} , ARE HIRE..</span>
+            </div>
+            </div>
+        </div>
     </div> 
   )
 }
--- src/Config/Colors.js
+++ src/Config/Colors.js
@@ -1,6 +1,17 @@
 
 const MAIN_COLOR = "#b091ff"
 
+const COLORS = [
+    "#FFB6C1", 
+    "#ADD8E6",  
+    "#98FB98", 
+    "#E6E6FA", 
+    "#FFDAB9", 
+    "#AFEEEE", 
+    "#FFE4E1", 
+    "#FAFAD2", 
+    "#F0FFF0"  
+  ]
 
 
-export {MAIN_COLOR}
+export {MAIN_COLOR ,COLORS}
