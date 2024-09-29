fafebe35686d580b9f7f74c53c9b5d243ae40598
sarrthak008 <sarthaknavale08@gmail.com>
2024-09-29 19:15:13 +0530

    add login code

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginForm.css"
-import { Link } from 'react-router-dom'
+import { json, Link, useNavigate } from 'react-router-dom'
 import LOGINIMG from './Login.png'
+import toast from 'react-hot-toast'
+
 
 const LoginForm = () => {
+     
+ const [email,setEmail] = useState('')
+ const [password,setPassword] = useState('') 
+ const [LOGINUSER ,setLOGINUSER] = useState(null)
+ const USERS = JSON.parse(localStorage.getItem("USERS")) || []
+ const navigate =useNavigate()
+
+  const handleLogin = ()=>{
+      USERS.forEach((USER)=> {
+        if(USER.email === email && USER.password === password){
+             setLOGINUSER(USER)
+        }
+      });
+
+    if(LOGINUSER){
+      toast.success('login successfully.')
+      localStorage.setItem("LOGINUSER",JSON.stringify(LOGINUSER))
+      navigate('/search')
+    }else{
+      toast.error('please check details.')
+    }
+  }
+
   return (
     <>
       <div className='login-form-container'>
       <span className='signup-text'>Login with workZone.</span>
           <div className='login-inputs-container'>
-            <input placeholder='your email id'  className='signup-input'></input>
-            <input placeholder='set password'  className='signup-input'></input>
-            <button className='btn1 signup-btn login-btn'>login</button>
+            <input placeholder='your email id'  className='signup-input' value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <input placeholder='set password'  className='signup-input' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button className='btn1 signup-btn login-btn' onClick={()=>{
+              handleLogin()
+            }}>login</button>
           </div>
         <Link to='/signup' className='link'> <span className='login-link'>i dont have an account ? signup</span> </Link>
       </div>
