f5a2e062043adaf589249057b4f8e9371ff160c5
sarrthak008 <sarthaknavale08@gmail.com>
2024-10-02 11:29:01 +0530

    add profile page

--- src/Views/Profile/Profile.js
+++ src/Views/Profile/Profile.js
@@ -4,11 +4,25 @@ import Sidebar from '../../Components/Sidebar/Sidebar'
 import Tagline from '../../Components/TagLine/Tagline'
 
 const Profile = () => {
+ 
+  const LOGINUSER = JSON.parse(localStorage.getItem('LOGINUSER'))
+  const {name ,lastName ,email,mobNumber} = LOGINUSER
   return (
     <>
     <Sidebar/>
     <div className='Profile-container' >
-            <div className='profile-main-card'></div>
+            <div className='profile-main-card'>
+                 <div className='profile-img-container'></div>
+                 <div className='prfile-user-info'>
+                   <span className='user-info'>{`${name} ${lastName} .`}</span>
+                   <span className='user-info gray'>{email}</span>
+                   <span className='user-info gray'>{mobNumber}</span>
+                 </div>
+                <div className='profile-setting'>
+                   <span><i class="ri-share-fill"></i></span>
+                   <span><i class="ri-tools-fill"></i></span>
+                  </div> 
+            </div>
             <Tagline tagline={'my apply'}/>
     </div>
     </>
