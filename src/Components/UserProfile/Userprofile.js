eb4153631307433900e501d86fc7ba7dbfb3f0d6
sarrthak008 <sarthaknavale08@gmail.com>
2024-10-05 14:46:16 +0530

    add github and linkdin icons

--- src/Components/UserProfile/Userprofile.js
+++ src/Components/UserProfile/Userprofile.js
@@ -6,20 +6,23 @@ import USER_IMG from './user.png'
 const Userprofile = () => {
     
   const LOGINUSER = JSON.parse(localStorage.getItem('LOGINUSER'))
-  const PROFILE_IMG = localStorage.getItem('PROFILE_IMG') || USER_IMG
+  const PROFILE_IMG = localStorage.getItem('PROFILE_IMG') || USER_IMG 
   const {name ,lastName ,email,mobNumber} = LOGINUSER
   const [pPopup,setpPopup] = useState(false)
+    
   return (
 
     <div>
       <div className='profile-main-card'>
                  <div className='profile-img-container'>
                      <img src={PROFILE_IMG} alt='profile image'></img>
+                     {console.log(PROFILE_IMG)}
                  </div>
                  <div className='prfile-user-info'>
                    <span className='user-info'>{`${name} ${lastName} .`}</span>
                    <span className='user-info gray'>{email}</span>
                    <span className='user-info gray'>{mobNumber}</span>
+                   <span className='user-info social-media'> {LOGINUSER.github_URL.length>0 ? <><a href={LOGINUSER.github_URL} target='__blank' className='link'><i class="ri-github-fill"></i></a> </>: null}  {LOGINUSER.linkdin_URL.length>0 ? <><a href={LOGINUSER.linkdin_URL} target='__blank' className='link'><i class="ri-linkedin-box-fill"></i></a> </>: null}</span>
                  </div>
                  <div className='profile-setting'>
                    <span><i class="ri-share-fill"></i></span>
